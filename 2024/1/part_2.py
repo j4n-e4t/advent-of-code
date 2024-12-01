99a41856eaebc5ad4e70f36e34d4a02c3025194a
Julian <130256240+j4n-e4t@users.noreply.github.com>
2024-12-01 11:06:15 +0100

    my code was fixed

--- 2024/1/part_2.py
+++ 2024/1/part_2.py
@@ -7,9 +7,6 @@ with open("input.txt") as f:
         left, right = line.strip().split("   ")
         left_list.append(left)
         right_list.append(right)
-    left_list.sort()
-    right_list.sort()
-
 
 for i in range(len(left_list)):
     similarities.append(int(left_list[i]) * right_list.count(left_list[i]))
