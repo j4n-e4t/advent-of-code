06c2000784488c78fc8b79c736e29171dcc9c068
Julian <130256240+j4n-e4t@users.noreply.github.com>
2024-12-01 09:44:03 +0100

    update new_day script

--- scripts/new_day.py
+++ scripts/new_day.py
@@ -1,6 +1,23 @@
-# get current year
-# get current day
-# create folder for year
-# create folder for day
-# create input_1.txt and inpput_2.txt
-# create part_1.py and part_2.py
+import os
+from datetime import datetime
+
+year = datetime.now().year
+day = datetime.now().day
+
+if not os.path.exists(f"{year}"):
+    os.mkdir(f"{year}")
+
+if not os.path.exists(f"{year}/{day}"):
+    os.mkdir(f"{year}/{day}")
+
+if not os.path.exists(f"{year}/{day}/input.txt"):
+    open(f"{year}/{day}/input.txt", "w").close()
+
+if not os.path.exists(f"{year}/{day}/input_test.txt"):
+    open(f"{year}/{day}/input_test.txt", "w").close()
+
+if not os.path.exists(f"{year}/{day}/part_1.py"):
+    open(f"{year}/{day}/part_1.py", "w").close()
+
+if not os.path.exists(f"{year}/{day}/part_2.py"):
+    open(f"{year}/{day}/part_2.py", "w").close()
